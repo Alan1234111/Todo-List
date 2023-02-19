c1eefadee413feb6bdedef77ab77ff8c2571d10d
Alan Pason <alan.pason132@gmail.com>
2023-02-19 16:16:39 +0100

    Feat: You can change bettwen projects

--- src/modules/Projects.js
+++ src/modules/Projects.js
@@ -2,8 +2,7 @@ import Task from "./Task";
 
 export default class Projects {
   constructor() {
-    const task = new Task();
-
+    this.task = new Task();
     this.btnAddProject = document.querySelector(".button-project-add");
 
     this.projectAddSection = document.querySelector(".popup-project-add");
@@ -12,24 +11,45 @@ export default class Projects {
     this.projectSectionCancelBtn = document.querySelector(".popup-button-cancel");
 
     this.userProjectsList = document.querySelector(".projects-list");
+    this.btnsDeafultsProjects = document.querySelectorAll(".button-deafult-project");
+    this.btnsUserProjects = document.querySelectorAll(".button-project");
 
     this.btnAddProject.addEventListener("click", this.toggleInputProjectAdd);
-
     this.projectSectionAddBtn.addEventListener("click", this.addProject);
     this.projectSectionCancelBtn.addEventListener("click", this.toggleInputProjectAdd);
+
+    this.btnsDeafultsProjects.forEach((btn) => btn.addEventListener("click", this.changeProject));
+    this.btnsUserProjects.forEach((btn) => btn.addEventListener("click", this.changeProject));
   }
 
-  //   createProjectBtn() {
-  // <button class="button-project"><img src="img/task-list.svg" aria-label="none" />Create To Do App</button>
+  createProjectBtn(projectName) {
+    const project = document.createElement("button");
+    project.classList.add("button-project");
 
-  //   }
+    project.innerHTML = `<img src="img/task-list.svg" aria-label="none"/> ${projectName}`;
+    project.addEventListener("click", this.changeProject);
+
+    return project;
+  }
 
   addProject = () => {
-    if (!this.projectSectionInput.value) return;
+    const projectName = this.projectSectionInput.value;
 
-    createProjectBtn();
+    if (!projectName) return;
+
+    this.userProjectsList.appendChild(this.createProjectBtn(projectName));
 
     this.toggleInputProjectAdd();
+  };
+
+  changeProject = (e) => {
+    this.btnsUserProjects = document.querySelectorAll(".button-project");
+    this.btnsDeafultsProjects.forEach((btn) => btn.classList.remove("active"));
+    this.btnsUserProjects.forEach((btn) => btn.classList.remove("active"));
+    e.target.classList.add("active");
+
+    this.task.activeProject = e.target.textContent;
+    this.task.renderTasks();
   };
 
   toggleInputProjectAdd = () => {
