d794d146e54107b9bde9afbec4860782c42a39f6
Alan Pason <alan.pason132@gmail.com>
2023-02-19 19:56:15 +0100

    Import: projects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Ui from "./modules/UI";
 import Task from "./modules/Task";
+import Projects from "./modules/Projects";
 
 init();
 
 function init() {
   const ui = new Ui();
-  const task = new Task();
-  
+  // const task = new Task();
+  const Project = new Projects();
+
   ui.init();
 }
