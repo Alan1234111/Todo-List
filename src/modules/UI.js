a222ccbb4afd7c1bbb2e57a8443abce951a19c8c
Alan Pason <alan.pason132@gmail.com>
2023-02-26 19:12:23 +0100

    Add: screenDiminning function

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,10 +2,23 @@ export default class Ui {
   init() {
     this.toggleSidebar = document.querySelector(".toggle-menu");
     this.toggleSidebar.addEventListener("click", this.openSidebar);
+    this.sidebar = document.querySelector(".sidebar");
   }
 
-  openSidebar() {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.classList.toggle("sidebar-active");
-  }
+  openSidebar = () => {
+    if (!this.toggleSidebar) {
+      const toggleSidebar = document.querySelector(".toggle-menu");
+      const sidebar = document.querySelector(".sidebar");
+      toggleSidebar.checked = false;
+      sidebar.classList.toggle("sidebar-active");
+      return;
+    }
+
+    this.sidebar.classList.toggle("sidebar-active");
+  };
+
+  screenDimming = () => {
+    const popupScreen = document.querySelector(".popup-screen");
+    popupScreen.classList.toggle("show");
+  };
 }
