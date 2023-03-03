18992babd1b8ee1868c9df647bf768dd75ac24ec
--unset <--unset>
2023-03-03 10:07:57 +0100

    Remove: export Tasks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import Ui from "./modules/UI";
-import Task from "./modules/Task";
 import Projects from "./modules/Projects";
-// import Storage from "./modules/Storage";
 
 init();
 
 function init() {
-  // const storage = new Storage();
   const ui = new Ui();
-  // const task = new Task();
   const Project = new Projects();
 
   ui.init();
